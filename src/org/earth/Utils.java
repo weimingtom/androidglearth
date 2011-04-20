04e5e79de31476c11e4f6e6680fec249de4451fc
nic.castel <nic.castel@gmail.com>
2011-04-20 23:34:27 +0000

    patch du parseur WMS

--- src/org/earth/Utils.java
+++ src/org/earth/Utils.java
@@ -4,13 +4,13 @@ public class Utils {
 	public static final float LN2 = 0.6931471805599453f;
 
 	public static float standardLongitudeRadians(float lon) {
-		float standard = Utils.modulo(lon, 2 * Math.PI);
+		float standard = Utils.modulo(lon, (float) (2 * Math.PI));
 		return (float) (standard > Math.PI ? standard - 2 * Math.PI : standard);
 	}
 
-	private static float modulo(float lon, double d) {
-		// TODO Auto-generated method stub
-		return 0;
+	public static float modulo(float a, float b) {
+		// TODO
+		return a % b;
 	}
 
 	public static float clamp(float latitude, double d, double e) {
@@ -26,6 +26,11 @@ public class Utils {
 	public static float[] flatten(float[][] metaBuffer) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public static void splice(float[] fs, int i, int diffX) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
