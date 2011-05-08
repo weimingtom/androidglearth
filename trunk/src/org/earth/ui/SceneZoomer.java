79c3450bc3104cc052d88277949d7d635158f99f
nic.castel@gmail.com <nic.castel@gmail.com@b3146043-c2a4-6a5b-d80a-1dc04946d65c>
2011-05-08 21:52:06 +0000

    Add touch pan

--- trunk/src/org/earth/ui/SceneZoomer.java
+++ trunk/src/org/earth/ui/SceneZoomer.java
@@ -1,5 +1,18 @@
 package org.earth.ui;
 
+import org.earth.scene.Scene;
+
+import android.view.MotionEvent;
+
 public class SceneZoomer {
+
+	public SceneZoomer(Scene scene) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void handleTouch(MotionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
